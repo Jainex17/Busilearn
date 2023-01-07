ddbe92dadc6c13ee7e41c98d733b47e31b58231e
jainex17 <jainexp017@gmail.com>
2023-01-07 19:51:05 +0530

    nav add catagory and add build folder

--- src/Componets/Navbar.js
+++ src/Componets/Navbar.js
@@ -3,23 +3,49 @@ import '../scss/Nav&footer.scss';
 
 function Navbar(){
     function menuclick(){
+        const navbar = document.querySelector('.navbar');
         const nav = document.querySelector('.res-nav');
-        console.log(nav);
+
+        navbar.classList.toggle('navbaropen');
         nav.classList.toggle('res-nav-open');
         nav.classList.toggle('res-nav-close');
     }
+    // function navcatagory(){
+    //     const catmenu = document.querySelector('.catagory-menu');
+    //     // catagorytitle.addEventListener('focus',()=>{
+    //     //     console.log('yoo');
+    //     // })
+    //     console.log('outside');
+    //     catmenu.classList.toggle('catagory-menu-open')
+
+    // }
     return(
         <div>
             <nav>
-                <section className='navbar'>
+                <header className='navbar'>
+                    <section className='navbar-right'>
                     <div className='logo'>
                         <h1>Busilearn</h1>
                         {/* <img src={logo}></img> */}
+                    </div>
+                    <div className='nav-menu-wapper'>
+                        <button type="button" className='nav-menu-title'><span>Categories</span></button>
+                                <div className='catagory-menu'>
+                                    <ul>
+                                        <li><button><span>Entrepreneurship</span><i className="fa-solid fa-angle-right"></i></button></li>
+                                        <li><button><span>Communication</span><i className="fa-solid fa-angle-right"></i></button></li>
+                                        <li><button><span>Sales</span><i className="fa-solid fa-angle-right"></i></button></li>
+                                        <li><button><span>Project Management</span><i className="fa-solid fa-angle-right"></i></button></li>
+                                        <li><button><span>E-Commerce</span><i className="fa-solid fa-angle-right"></i></button></li>
+                                    </ul>
+                                </div>
                     </div>
                     <div className='nav-search'>
                     <i className="fa-solid fa-magnifying-glass"></i>
                         <input type={'text'} placeholder="Search for anyting" />
                     </div>
+                    </section>
+                    <section className='navbar-left'>
                     <div className='nav-links'>
                         <ul>
                             <li><a href='#'>Teach with us</a></li>
@@ -30,10 +56,12 @@ function Navbar(){
                     <div className='res-nav-btn' onClick={menuclick}>
                         <i className="fa-solid fa-bars"></i>
                     </div>
-                </section>
+                    </section>
+                </header>
                     <div className='res-nav res-nav-close'>
                         <div className='res-search'>
-                            <input type={'text'}/>
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                            <input type={'text'} placeholder="Search anything"/>
                         </div>
                     </div>
             </nav>
