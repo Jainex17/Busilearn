6dfca5e6ddc84c200be97a3c87e30542c16941a4
jainex17 <jainexp017@gmail.com>
2023-02-14 20:24:22 +0530

    footer style

--- src/Componets/Footer/footer.js
+++ src/Componets/Footer/footer.js
@@ -3,27 +3,41 @@ import { Link } from "react-router-dom";
 import "./footer.scss";
 
 function Footer() {
+    let today = new Date();
     return(
         <footer>
-            <div className="footer-logo">
+            {/* <div className="footer-logo">
                 <h1>Busilearn</h1>
-            </div>
-            <div className="footer-links">
-                <div className="footer-links-c1">
+            </div> */}
+            <div className="footer-sections">
+                <div className="footer-section-c1">
+                    <h5>Links</h5>
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/">Contact Us</Link></li>
                         <li><Link to="/">About Us</Link></li>
                     </ul>
                 </div>
-                <div className="footer-links-c2">
-                    <li><Link to="/">Teach with us</Link></li>
-                    <li><Link to="/"></Link></li>
+                <div className="footer-section-c2">
+                    <h1>BUSILEARN</h1>
+                    <input type={"email"} placeholder="Your Email Adress" />
+                    <button>Subscribe</button>
+                </div>
+                <div className="footer-section-c3">
+                    <h5>Contact</h5>
+                    <p>125, Busilearn Adajan</p>
+                    <p>Surat , Gujarat , IN</p>
+                    <div className="footer-social">
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
                 </div>
             </div>
-            <div className="footer-copyright">
-                <p>© 2023 Udemy, Inc.</p>
-            </div>
+            {/* <div className="footer-copyright">
+                <p>© <span>{today.getFullYear()}</span> Busilearn, Inc.</p>
+            </div> */}
         </footer>
     )
 }
