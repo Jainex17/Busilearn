1c048604f80ccd71001e2f80368ef882a94837b7
jainex17 <jainexp017@gmail.com>
2023-02-15 22:18:00 +0530

    redux implementation

---
+++ src/redux/actions/user.js
@@ -0,0 +1,17 @@
+import {server} from '../store';
+import axios from 'axios';
+
+const login = (email,password) => async (dispatch) => {
+    try{
+        dispatch({type:"loginrequest"});
+ 
+        const {data} = await axios.post(`${server}/login`,{email,password},{
+            headers:{
+                "Content-Type":"application/json",
+            },withCredentials:true,
+        });
+        dispatch({type:"loginsucess"});
+    }catch(error){
+        dispatch({type:"loginfail",payload:error.response.data.message});
+    }
+}
---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,25 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+export const userReducer = createReducer({},{
+    loginReduest:(state)=>{
+        state.loading=true;
+    },
+    loginSucess:(state,action)=>{
+        state.loding=false;
+        state.isAuthenicated=true;
+        state.user=action.payload;
+        state.message=action.payload.message;
+    },
+    loginFail:(state,action)=>{
+        state.loding = false;
+        state.isAuthenicated = false;
+        state.error = action.payload;
+    },
+    clearError:(state)=>{
+        state.error=null;
+    },
+    clearmessage:(state)=>{
+        state.message=null;
+    }
+
+})
