8893e8280ff2c2cf534d26ff629fc06e30c984bb
jainex17 <jainexp017@gmail.com>
2023-02-16 21:33:34 +0530

    dictionary manage,footer style

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,8 +1,8 @@
-import Navbar from "../../Componets/Navbar/Navbar";
+import Navbar from "../../Componets/layout/Navbar/Navbar";
 import Banner from "../../Componets/Herosection/Hero-section";
 import Courselist from "../../Componets/Courselist/Courselist";
-import Teachwithus from "../../Componets/Teachwithus/Teachwithus";
-import Footer from "../../Componets/Footer/footer";
+import Teachwithus from "../../Componets/layout/Banner-Teachwithus/Teachwithus";
+import Footer from "../../Componets/layout/Footer/footer";
 
 function Homepage() {
   return (
